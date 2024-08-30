c8d28b6ff150e8c5e147cd9d48a2ced7bd43ecf5
Mahmudul Haque Noor <mahmudulhaquenoor@gmail.com>
2024-08-30 11:42:25 +0600

    spinner component created

--- src/pages/services/Services.tsx
+++ src/pages/services/Services.tsx
@@ -5,6 +5,7 @@ import { FiFilter } from 'react-icons/fi'
 import FilterCard from '../../components/cards/FilterCard'
 import { Link } from 'react-router-dom'
 import { useGetAllServiceQuery } from '../../redux/features/service/serviceApi'
+import Spinner from '../../components/spinner/Spinner'
 
 const Services = () => {
   const [capacity, setCapacity] = useState(10)
@@ -12,8 +13,7 @@ const Services = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   // getting services data
-  const { data: serviceData } = useGetAllServiceQuery(undefined)
-  console.log(serviceData)
+  const { data: serviceData, isLoading } = useGetAllServiceQuery(undefined)
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen)
@@ -25,6 +25,58 @@ const Services = () => {
 
   const onChange = (e: RadioChangeEvent) => {
     setSortOrder(e.target.value)
+  }
+
+  let content
+
+  if (isLoading) {
+    content = <Spinner />
+  } else {
+    content = (
+      <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
+        {serviceData?.data?.map((service) => (
+          <div
+            key={service?._id}
+            className='rounded-lg bg-white p-4 shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl flex flex-col'>
+            <div className='relative rounded-lg overflow-hidden'>
+              <img
+                src={service?.image}
+                alt='Car Wash Service'
+                className='w-full h-48 object-cover'
+              />
+              <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-75'></div>
+              <div className='absolute bottom-4 left-4'>
+                <span className='text-white text-sm font-semibold bg-primary-500 px-3 py-1 rounded-md'>
+                  {service?.duration} Minutes
+                </span>
+              </div>
+            </div>
+
+            <div className='flex flex-col flex-grow mt-4'>
+              <h2 className='text-xl font-semibold text-primary-900'>
+                {service?.name}
+              </h2>
+              <p
+                className='text-primary-700 mt-2 mb-4 flex-grow truncate overflow-hidden'
+                style={{ maxHeight: '3rem' }}>
+                {service?.description}
+              </p>
+
+              <div className='flex flex-col items-center mt-auto'>
+                <p className='text-lg font-bold text-secondary-600 mb-2'>
+                  ৳{service?.price}
+                </p>
+                <Link to={'/service-details'} className='w-full'>
+                  <button className='w-full bg-primary-500 text-white py-2 px-4 rounded-md text-center hover:bg-primary-600 transition-colors'>
+                    See Details
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    )
   }
 
   return (
@@ -175,49 +227,7 @@ const Services = () => {
         </div>
 
         {/* products section */}
-        <div className='flex-1 rounded-md mx-auto'>
-          <div className='grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-            {serviceData?.data?.map((service) => (
-              <div className='rounded-lg bg-white p-4 shadow-lg transform transition-transform duration-300 hover:scale-105 hover:shadow-xl flex flex-col'>
-                <div className='relative rounded-lg overflow-hidden'>
-                  <img
-                    src={service?.image}
-                    alt='Car Wash Service'
-                    className='w-full h-48 object-cover'
-                  />
-                  <div className='absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-75'></div>
-                  <div className='absolute bottom-4 left-4'>
-                    <span className='text-white text-sm font-semibold bg-primary-500 px-3 py-1 rounded-md'>
-                      {service?.duration} Minutes
-                    </span>
-                  </div>
-                </div>
-
-                <div className='flex flex-col flex-grow mt-4'>
-                  <h2 className='text-xl font-semibold text-primary-900'>
-                    {service?.name}
-                  </h2>
-                  <p
-                    className='text-primary-700 mt-2 mb-4 flex-grow truncate overflow-hidden'
-                    style={{ maxHeight: '3rem' }}>
-                    {service?.description}
-                  </p>
-
-                  <div className='flex flex-col items-center mt-auto'>
-                    <p className='text-lg font-bold text-secondary-600 mb-2'>
-                      ৳{service?.price}
-                    </p>
-                    <Link to={'/service-details'} className='w-full'>
-                      <button className='w-full bg-primary-500 text-white py-2 px-4 rounded-md text-center hover:bg-primary-600 transition-colors'>
-                        See Details
-                      </button>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
+        <div className='flex-1 rounded-md mx-auto'>{content}</div>
       </div>
     </div>
   )
